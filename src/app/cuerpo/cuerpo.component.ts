ed34fc8f9e0a316f5f3818adf32ba973da0da499
AndreaMCH24 <aome2005.kirara7@gmail.com>
2024-05-31 19:21:21 -0600

    Andreaparte

--- src/app/cuerpo/cuerpo.component.ts
+++ src/app/cuerpo/cuerpo.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cuerpo',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cuerpo.component.html',
   styleUrl: './cuerpo.component.css'
 })
 export class CuerpoComponent {
+
+  title = 'Conteo de Precios';
+  cantidades: number[] = [0,0,0,0,0];
+  precios: number[] = [250,150,175,120,140];
+  totalPrecio: number=0;
+  valor2:number=0;
+
+  calculateTotal() {
+    this.totalPrecio = this.cantidades.reduce((total, qty, index) => total + qty * this.precios[index], 0);
+  }
+
 
 }
